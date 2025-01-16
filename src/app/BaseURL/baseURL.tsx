df3f22a05c8bb95a1c8d7deea67b5d117aad47b4
Ralpmike <ralphmike996@gmail.com>
2025-01-16 16:50:14 +0100

    fix: replace endpoints for the forms

--- src/app/BaseURL/baseURL.tsx
+++ src/app/BaseURL/baseURL.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://futurelabs-manager-be.onrender.com/api/v1',
+  baseURL: 'https://futurelabs-manager-be-api.onrender.com/api/v1',
   headers: {
     'x-api-key': 'NKa4Do2rjKnhYhmHHXIyw9nGEG3o7fNvCGoS9s0VFRQ',
   },
