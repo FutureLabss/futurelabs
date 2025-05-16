e3adf8f93a740af4d07ee95c43279068ae168966
PeroHub <peterime600@gmail.com>
2025-05-16 11:31:06 +0100

    f

--- src/app/components/talent/TalentPool.tsx
+++ src/app/components/talent/TalentPool.tsx
@@ -58,7 +58,7 @@ export default function TalentPool() {
               </p>
               <Link
                 target="_blank"
-                href="/services/hiretalent/talent"
+                href="/services/learnskill"
                 className="block mx-auto md:mx-0 text-primary bg-secondary-default/20 px-3 py-1  rounded text-secondary-default  font-semibold w-fit"
               >
                 Hire Talent
